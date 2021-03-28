2dc7203c21ff2e8678ce5007f01dc3403312df56
Maximilian Schott <maximilian.schott@roottec.com>
2021-03-28 06:22:11 +0200

    refactor!(database): removed the need for convertAssign feat(database): added NullInt16

--- database/dbtypes/nullabletypes.go
+++ database/dbtypes/nullabletypes.go
@@ -2,257 +2,529 @@ package dbtypes
 
 import (
 	"database/sql/driver"
-	"strconv"
+	"encoding/json"
+
+	convert "github.com/Eun/go-convert"
 )
 
-// NullString represents a string that may be null.
-// NullString implements the Scanner interface so
-// it can be used as a scan destination:
-//
-//  var s NullString
-//  err := db.QueryRow("SELECT name FROM foo WHERE id=?", id).Scan(&s)
-//  ...
-//  if s.Valid {
-//     // use s.String
-//  } else {
-//     // NULL value
-//  }
-//
-// NullString implements the encoding.json.Mashaller interface to properly
-// support json null
+/*
+	NullString represents a string that may be null.
+
+	NullString implements
+	- encoding/json.Mashaller
+	- encoding/json.Unmarshaller
+	- sql.Scanner
+	- sql/driver.Valuer
+
+	Example Usage:
+
+		var s NullString
+		err := db.QueryRow("SELECT name FROM foo WHERE id=?", id).Scan(&s)
+		...
+		if !s.IsNull() {
+		  // use s.Get()
+		} else {
+		  // NULL value
+		}
+*/
 type NullString struct {
-	String string
-	Valid  bool // Valid is true if String is not NULL
+	str  string
+	null bool
 }
 
-// Scan implements the Scanner interface.
-func (this *NullString) Scan(value interface{}) error {
-	if value == nil {
-		this.String, this.Valid = "", false
-		return nil
+func (n NullString) Set(v *string) {
+	if v == nil {
+		n.str = ""
+		n.null = true
+	} else {
+		n.str = (*v)[:]
+		n.null = false
 	}
-	this.Valid = true
-	return convertAssign(&this.String, value)
 }
 
-// Value implements the driver Valuer interface.
+func (n NullString) Get() string {
+	return n.str
+}
+
+func (n NullString) IsNull() bool {
+	return n.null
+}
+
+func (this *NullString) Scan(value interface{}) error {
+	var err error
+	if value != nil {
+		var v string
+		err = convert.Convert(value, &v)
+		if err == nil {
+			this.Set(&v)
+			return nil
+		}
+	}
+	this.Set(nil)
+	return err
+}
+
 func (this NullString) Value() (driver.Value, error) {
-	if !this.Valid {
+	if this.IsNull() {
 		return nil, nil
 	}
-	return this.String, nil
+	return this.Get(), nil
 }
 
 func (this NullString) MarshalJSON() ([]byte, error) {
-	if this.Valid {
-		return []byte(`"` + this.String + `"`), nil
+	if this.IsNull() {
+		var v interface{} = nil
+		return json.Marshal(v)
 	} else {
-		return []byte("null"), nil
+		return json.Marshal(this.Get())
 	}
 }
 
-func (this NullString) Set(value string) {
-	this.String = value
-	this.Valid = true
+func (this NullString) UnmarshalJSON(bytes []byte) error {
+	var v *string
+	err := json.Unmarshal(bytes, &v)
+	if err == nil {
+		this.Set(v)
+	} else {
+		this.Set(nil)
+	}
+	return err
 }
 
-// NullInt64 represents an int64 that may be null.
-// NullInt64 implements the Scanner interface so
-// it can be used as a scan destination, similar to NullString.
-// NullInt64 implements the encoding.json.Mashaller interface to properly
-// support json null
+/*
+	NullInt64 represents an int64 that may be null.
+
+	NullInt64 implements
+	- encoding/json.Mashaller
+	- encoding/json.Unmarshaller
+	- sql.Scanner
+	- sql/driver.Valuer
+*/
 type NullInt64 struct {
-	Int64 int64
-	Valid bool // Valid is true if Int64 is not NULL
+	value int64
+	null  bool
 }
 
-// Scan implements the Scanner interface.
-func (this *NullInt64) Scan(value interface{}) error {
-	if value == nil {
-		this.Int64, this.Valid = 0, false
-		return nil
+func (this NullInt64) Set(v *int64) {
+	if v == nil {
+		this.value = 0
+		this.null = true
+	} else {
+		this.value = *v
+		this.null = false
 	}
-	this.Valid = true
-	return convertAssign(&this.Int64, value)
 }
 
-// Value implements the driver Valuer interface.
+func (this NullInt64) Get() int64 {
+	return this.value
+}
+
+func (this NullInt64) IsNull() bool {
+	return this.null
+}
+
+func (this *NullInt64) Scan(value interface{}) error {
+	var err error
+	if value != nil {
+		var v int64
+		err = convert.Convert(value, &v)
+		if err == nil {
+			this.Set(&v)
+			return nil
+		}
+	}
+	this.Set(nil)
+	return err
+}
+
 func (this NullInt64) Value() (driver.Value, error) {
-	if !this.Valid {
+	if this.IsNull() {
 		return nil, nil
 	}
-	return this.Int64, nil
+	return this.Get(), nil
 }
 
 func (this NullInt64) MarshalJSON() ([]byte, error) {
-	if this.Valid {
-		return []byte(strconv.FormatInt(this.Int64, 10)), nil
+	if this.IsNull() {
+		var v interface{} = nil
+		return json.Marshal(v)
 	} else {
-		return []byte("null"), nil
+		return json.Marshal(this.Get())
 	}
 }
 
-func (this NullInt64) Set(value int64) {
-	this.Int64 = value
-	this.Valid = true
+func (this NullInt64) UnmarshalJSON(bytes []byte) error {
+	var v *int64
+	err := json.Unmarshal(bytes, &v)
+	if err == nil {
+		this.Set(v)
+	} else {
+		this.Set(nil)
+	}
+	return err
 }
 
-// NullInt32 represents an int64 that may be null.
-// NullInt32 implements the Scanner interface so
-// it can be used as a scan destination, similar to NullString.
-// NullInt32 implements the encoding.json.Mashaller interface to properly
-// support json null
+/*
+	NullInt32 represents an int64 that may be null.
+
+	NullInt64 implements
+	- encoding/json.Mashaller
+	- encoding/json.Unmarshaller
+	- sql.Scanner
+	- sql/driver.Valuer
+*/
 type NullInt32 struct {
-	Int32 int32
-	Valid bool // Valid is true if Int32 is not NULL
+	value int32
+	null  bool
 }
 
-// Scan implements the Scanner interface.
-func (n *NullInt32) Scan(value interface{}) error {
-	if value == nil {
-		n.Int32, n.Valid = 0, false
-		return nil
+func (this NullInt32) Set(v *int32) {
+	if v == nil {
+		this.value = 0
+		this.null = true
+	} else {
+		this.value = *v
+		this.null = false
 	}
-	n.Valid = true
-	return convertAssign(&n.Int32, value)
 }
 
-// Value implements the driver Valuer interface.
+func (this NullInt32) Get() int32 {
+	return this.value
+}
+
+func (this NullInt32) IsNull() bool {
+	return this.null
+}
+
+func (this *NullInt32) Scan(value interface{}) error {
+	var err error
+	if value != nil {
+		var v int32
+		err = convert.Convert(value, &v)
+		if err == nil {
+			this.Set(&v)
+			return nil
+		}
+	}
+	this.Set(nil)
+	return err
+}
+
 func (this NullInt32) Value() (driver.Value, error) {
-	if !this.Valid {
+	if this.IsNull() {
 		return nil, nil
 	}
-	return this.Int32, nil
+	return this.Get(), nil
 }
 
 func (this NullInt32) MarshalJSON() ([]byte, error) {
-	if this.Valid {
-		return []byte(strconv.FormatInt(int64(this.Int32), 10)), nil
+	if this.IsNull() {
+		var v interface{} = nil
+		return json.Marshal(v)
 	} else {
-		return []byte("null"), nil
+		return json.Marshal(this.Get())
 	}
 }
 
-func (this NullInt32) Set(value int32) {
-	this.Int32 = value
-	this.Valid = true
-}
-
-// NullInt8 represents an int64 that may be null.
-// NullInt8 implements the Scanner interface so
-// it can be used as a scan destination, similar to NullString.
-// NullInt8 implements the encoding.json.Mashaller interface to properly
-// support json null
-type NullInt8 struct {
-	Int8  int8
-	Valid bool // Valid is true if Int32 is not NULL
-}
-
-// Scan implements the Scanner interface.
-func (n *NullInt8) Scan(value interface{}) error {
-	if value == nil {
-		n.Int8, n.Valid = 0, false
-		return nil
+func (this NullInt32) UnmarshalJSON(bytes []byte) error {
+	var v *int32
+	err := json.Unmarshal(bytes, &v)
+	if err == nil {
+		this.Set(v)
+	} else {
+		this.Set(nil)
 	}
-	n.Valid = true
-	return convertAssign(&n.Int8, value)
+	return err
 }
 
-// Value implements the driver Valuer interface.
-func (this NullInt8) Value() (driver.Value, error) {
-	if !this.Valid {
+/*
+	NullInt16 represents an int16 that may be null.
+
+	NullInt64 implements
+	- encoding/json.Mashaller
+	- encoding/json.Unmarshaller
+	- sql.Scanner
+	- sql/driver.Valuer
+*/
+type NullInt16 struct {
+	value int16
+	null  bool
+}
+
+func (this NullInt16) Set(v *int16) {
+	if v == nil {
+		this.value = 0
+		this.null = true
+	} else {
+		this.value = *v
+		this.null = false
+	}
+}
+
+func (this NullInt16) Get() int16 {
+	return this.value
+}
+
+func (this NullInt16) IsNull() bool {
+	return this.null
+}
+
+func (this *NullInt16) Scan(value interface{}) error {
+	var err error
+	if value != nil {
+		var v int16
+		err = convert.Convert(value, &v)
+		if err == nil {
+			this.Set(&v)
+			return nil
+		}
+	}
+	this.Set(nil)
+	return err
+}
+
+func (this NullInt16) Value() (driver.Value, error) {
+	if this.IsNull() {
 		return nil, nil
 	}
-	return this.Int8, nil
+	return this.Get(), nil
+}
+
+func (this NullInt16) MarshalJSON() ([]byte, error) {
+	if this.IsNull() {
+		var v interface{} = nil
+		return json.Marshal(v)
+	} else {
+		return json.Marshal(this.Get())
+	}
+}
+
+func (this NullInt16) UnmarshalJSON(bytes []byte) error {
+	var v *int16
+	err := json.Unmarshal(bytes, &v)
+	if err == nil {
+		this.Set(v)
+	} else {
+		this.Set(nil)
+	}
+	return err
+}
+
+/*
+	NullInt8 represents an int64 that may be null.
+
+	NullInt64 implements
+	- encoding/json.Mashaller
+	- encoding/json.Unmarshaller
+	- sql.Scanner
+	- sql/driver.Valuer
+*/
+type NullInt8 struct {
+	value int8
+	null  bool // Valid is true if Int32 is not NULL
+}
+
+func (this NullInt8) Set(v *int8) {
+	if v == nil {
+		this.value = 0
+		this.null = true
+	} else {
+		this.value = *v
+		this.null = false
+	}
+}
+
+func (this NullInt8) Get() int8 {
+	return this.value
+}
+
+func (this NullInt8) IsNull() bool {
+	return this.null
+}
+
+func (this *NullInt8) Scan(value interface{}) error {
+	var err error
+	if value != nil {
+		var v int8
+		err = convert.Convert(value, &v)
+		if err == nil {
+			this.Set(&v)
+			return nil
+		}
+	}
+	this.Set(nil)
+	return err
+}
+
+func (this NullInt8) Value() (driver.Value, error) {
+	if this.IsNull() {
+		return nil, nil
+	}
+	return this.Get(), nil
 }
 
 func (this NullInt8) MarshalJSON() ([]byte, error) {
-	if this.Valid {
-		return []byte(strconv.FormatInt(int64(this.Int8), 10)), nil
+	if this.IsNull() {
+		var v interface{} = nil
+		return json.Marshal(v)
 	} else {
-		return []byte("null"), nil
+		return json.Marshal(this.Get())
 	}
 }
 
-func (this NullInt8) Set(value int8) {
-	this.Int8 = value
-	this.Valid = true
+func (this NullInt8) UnmarshalJSON(bytes []byte) error {
+	var v *int8
+	err := json.Unmarshal(bytes, &v)
+	if err == nil {
+		this.Set(v)
+	} else {
+		this.Set(nil)
+	}
+	return err
 }
 
-// NullFloat64 represents a float64 that may be null.
-// NullFloat64 implements the Scanner interface so
-// it can be used as a scan destination, similar to NullString.
+/*
+	NullFloat64 represents a float64 that may be null.
+
+	NullInt64 implements
+	- encoding/json.Mashaller
+	- encoding/json.Unmarshaller
+	- sql.Scanner
+	- sql/driver.Valuer
+*/
 type NullFloat64 struct {
-	Float64 float64
-	Valid   bool // Valid is true if Float64 is not NULL
+	value float64
+	null  bool // Valid is true if Float64 is not NULL
 }
 
-// Scan implements the Scanner interface.
-func (this *NullFloat64) Scan(value interface{}) error {
-	if value == nil {
-		this.Float64, this.Valid = 0, false
-		return nil
+func (this NullFloat64) Set(v *float64) {
+	if v == nil {
+		this.value = 0
+		this.null = true
+	} else {
+		this.value = *v
+		this.null = false
 	}
-	this.Valid = true
-	return convertAssign(&this.Float64, value)
 }
 
-// Value implements the driver Valuer interface.
+func (this NullFloat64) Get() float64 {
+	return this.value
+}
+
+func (this NullFloat64) IsNull() bool {
+	return this.null
+}
+
+func (this *NullFloat64) Scan(value interface{}) error {
+	var err error
+	if value != nil {
+		var v float64
+		err = convert.Convert(value, &v)
+		if err == nil {
+			this.Set(&v)
+			return nil
+		}
+	}
+	this.Set(nil)
+	return err
+}
+
 func (this NullFloat64) Value() (driver.Value, error) {
-	if !this.Valid {
+	if this.IsNull() {
 		return nil, nil
 	}
-	return this.Float64, nil
+	return this.Get(), nil
 }
 
 func (this NullFloat64) MarshalJSON() ([]byte, error) {
-	if this.Valid {
-		return []byte(strconv.FormatFloat(this.Float64, 'f', -1, 64)), nil
+	if this.IsNull() {
+		var v interface{} = nil
+		return json.Marshal(v)
 	} else {
-		return []byte("null"), nil
+		return json.Marshal(this.Get())
 	}
 }
 
-func (this NullFloat64) Set(value float64) {
-	this.Float64 = value
-	this.Valid = true
+func (this NullFloat64) UnmarshalJSON(bytes []byte) error {
+	var v *float64
+	err := json.Unmarshal(bytes, &v)
+	if err == nil {
+		this.Set(v)
+	} else {
+		this.Set(nil)
+	}
+	return err
 }
 
-// NullFloat64 represents a float64 that may be null.
-// NullFloat64 implements the Scanner interface so
-// it can be used as a scan destination, similar to NullString.
+/*
+	NullFloat64 represents a float64 that may be null.
+
+	NullInt64 implements
+	- encoding/json.Mashaller
+	- encoding/json.Unmarshaller
+	- sql.Scanner
+	- sql/driver.Valuer
+*/
 type NullFloat32 struct {
-	Float32 float32
-	Valid   bool
+	value float32
+	null  bool
 }
 
-// Scan implements the Scanner interface.
-func (n *NullFloat32) Scan(value interface{}) error {
-	if value == nil {
-		n.Float32, n.Valid = 0, false
-		return nil
+func (this NullFloat32) Set(v *float32) {
+	if v == nil {
+		this.value = 0
+		this.null = true
+	} else {
+		this.value = *v
+		this.null = false
 	}
-	n.Valid = true
-	return convertAssign(&n.Float32, value)
 }
 
-// Value implements the driver Valuer interface.
-func (n NullFloat32) Value() (driver.Value, error) {
-	if !n.Valid {
+func (this NullFloat32) Get() float32 {
+	return this.value
+}
+
+func (this NullFloat32) IsNull() bool {
+	return this.null
+}
+
+func (this *NullFloat32) Scan(value interface{}) error {
+	var err error
+	if value != nil {
+		var v float32
+		err = convert.Convert(value, &v)
+		if err == nil {
+			this.Set(&v)
+			return nil
+		}
+	}
+	this.Set(nil)
+	return err
+}
+
+func (this NullFloat32) Value() (driver.Value, error) {
+	if this.IsNull() {
 		return nil, nil
 	}
-	return n.Float32, nil
+	return this.Get(), nil
 }
 
 func (this NullFloat32) MarshalJSON() ([]byte, error) {
-	if this.Valid {
-		return []byte(strconv.FormatFloat(float64(this.Float32), 'f', -1, 64)), nil
+	if this.IsNull() {
+		var v interface{} = nil
+		return json.Marshal(v)
 	} else {
-		return []byte("null"), nil
+		return json.Marshal(this.Get())
 	}
 }
 
-func (this NullFloat32) Set(value float32) {
-	this.Float32 = value
-	this.Valid = true
+func (this NullFloat32) UnmarshalJSON(bytes []byte) error {
+	var v *float32
+	err := json.Unmarshal(bytes, &v)
+	if err == nil {
+		this.Set(v)
+	} else {
+		this.Set(nil)
+	}
+	return err
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ms-xy/go-common
 go 1.16
 
 require (
+	github.com/Eun/go-convert v1.2.12
 	github.com/Masterminds/squirrel v1.5.0
 	github.com/google/uuid v1.2.0
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0
